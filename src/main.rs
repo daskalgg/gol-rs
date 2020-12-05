407fa5f67e2457bd3c22112bb2e1dd2770aeb5da
George Daskalakis <daska@protonmail.com>
2020-12-05 12:23:05 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,24 @@ use rustbox::Key as RKey;
 use rustbox::{Color, RustBox};
 
 // use rand::{thread_rng, Rng};
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+// use rand::seq::SliceRandom;
+// use rand::thread_rng;
 
 struct Cell {
-    is_bomb: bool,
-    is_visible: bool,
-    is_marked: bool,
+    is_alive: bool,
+    was_alive: bool,
 }
 
 struct Board {
     grid: Vec<Vec<Cell>>,
     size: usize,
-    num_of_bombs: usize,
 }
 
 impl Board {
-    fn new(size: usize, num_of_bombs: usize) -> Self {
+    fn new(size: usize) -> Self {
         let mut board = Board {
             grid: Vec::new(),
             size: size,
-            num_of_bombs: num_of_bombs,
         };
 
         for _i in 0..size {
@@ -37,106 +34,74 @@ impl Board {
         for i in 0..size {
             for _j in 0..size {
                 board.grid[i].push(Cell {
-                    is_bomb: false,
-                    is_visible: false,
-                    is_marked: false,
+                    is_alive: false,
+                    was_alive: false,
                 });
             }
-        }
-
-        // TODO: insert bombs
-        let mut bombs: Vec<usize> = (0..size * size).collect();
-        let mut rng = thread_rng();
-        bombs.shuffle(&mut rng);
-
-        for index in 0..num_of_bombs {
-            let _index = bombs[index];
-            let i = _index / size;
-            let j = _index % size;
-            board.grid[i][j].is_bomb = true;
         }
 
         return board;
     }
 
-    // We don't chech if current cell is bomb. We should have checked for that when we selected it.
-    fn neighbour_bombs(&self, x: usize, y: usize) -> u8 {
-        let mut num_of_bombs: u8 = 0;
-        for i in (x as isize - 1)..(x as isize + 2) {
-            if i < 0 || i >= (self.size as isize) {
-                continue;
-            }
-            for j in (y as isize - 1)..(y as isize + 2) {
-                if j < 0 || j >= (self.size as isize) {
+    fn alive_cell_toggle(&mut self, x: usize, y: usize) {
+        self.grid[x][y].is_alive = !self.grid[x][y].is_alive;
+        self.grid[x][y].was_alive = self.grid[x][y].is_alive;
+    }
+
+    fn was_alive(&self, x: usize, y: usize) -> bool {
+        return self.grid[x][y].was_alive;
+    }
+
+    fn is_alive(&self, x: usize, y: usize) -> bool {
+        return self.grid[x][y].is_alive;
+    }
+
+    fn num_of_alive_neighbors(&self, x: isize, y: isize) -> usize {
+        let mut noan = 0;
+        for i in (x - 1)..(x + 2) {
+            for j in (y - 1)..(y + 2) {
+                if i == j
+                    || i < 0
+                    || i >= (self.size as isize)
+                    || j < 0
+                    || j >= (self.size as isize)
+                {
                     continue;
                 }
 
-                if i as usize == x && j as usize == y {
-                    continue;
-                }
-
-                if self.grid[i as usize][j as usize].is_bomb {
-                    num_of_bombs += 1;
+                if self.was_alive(i as usize, j as usize) {
+                    noan += 1;
                 }
             }
         }
-        return num_of_bombs;
+        return noan;
     }
 
-    fn reveal_cell(&mut self, x: usize, y: usize) {
-        if self.grid[x][y].is_visible {
-            return;
-        }
-
-        self.grid[x][y].is_visible = true;
-
-        if self.grid[x][y].is_bomb {
-            return;
-        }
-
-        if self.neighbour_bombs(x, y) != 0 {
-            return;
-        }
-
-        for i in (x as isize - 1)..(x as isize + 2) {
-            if i < 0 || i >= (self.size as isize) {
-                continue;
-            }
-            for j in (y as isize - 1)..(y as isize + 2) {
-                if j < 0 || j >= (self.size as isize) {
-                    continue;
-                }
-
-                self.reveal_cell(i as usize, j as usize);
+    fn backup(&mut self) {
+        for i in 0..self.size {
+            for j in 0..self.size {
+                self.grid[i][j].was_alive = self.grid[i][j].is_alive;
             }
         }
     }
 
-    fn chech_win(&self) -> bool {
-        let mut count = 0;
-
-        for i in 0..self.grid.len() {
-            for j in 0..self.grid.len() {
-                if !self.grid[i][j].is_visible {
-                    count += 1;
+    fn update(&mut self) {
+        self.backup();
+        for i in 0..self.size {
+            for j in 0..self.size {
+                let noan = self.num_of_alive_neighbors(i as isize, j as isize);
+                if noan < 2 || noan > 3 {
+                    self.grid[i][j].is_alive = false;
+                }
+                if noan == 3 {
+                    self.grid[i][j].is_alive = true;
+                }
+                if noan == 2 && self.is_alive(i, j) {
+                    self.grid[i][j].is_alive = true;
                 }
             }
         }
-
-        return count == self.num_of_bombs;
     }
-
-    fn mark_cell_toggle(&mut self, x: usize, y: usize) {
-        if !self.grid[x][y].is_visible {
-            self.grid[x][y].is_marked = !self.grid[x][y].is_marked;
-        }
-    }
-}
-
-enum Status {
-    WON,
-    LOST,
-    QUIT,
 }
 
 struct Game<'a> {
@@ -146,8 +111,8 @@ struct Game<'a> {
 }
 
 impl<'a> Game<'a> {
-    fn new(size: usize, num_of_bombs: usize, rustbox: &'a RustBox) -> Self {
-        let board = Board::new(size, num_of_bombs);
+    fn new(size: usize, rustbox: &'a RustBox) -> Self {
+        let board = Board::new(size);
 
         Game {
             rustbox: rustbox,
@@ -160,7 +125,7 @@ impl<'a> Game<'a> {
         let mut x = 0;
         let mut y = 0;
 
-        let mut fg = Color::White;
+        let fg = Color::White;
         let bg = Color::Black;
 
         let boarder = "+---";
@@ -187,19 +152,8 @@ impl<'a> Game<'a> {
                 let mut fg = Color::White;
                 let bg = Color::Black;
 
-                if !self.board.grid[i][j].is_visible {
+                if self.board.grid[i][j].is_alive {
                     ch = '█';
-                    if self.board.grid[i][j].is_marked {
-                        fg = Color::Yellow;
-                    }
-                } else {
-                    ch = (self.board.neighbour_bombs(i, j) + 48) as char; // Note: the 48 is there to make it compatible with ascii.
-
-                    if self.board.grid[i][j].is_bomb {
-                        ch = 'B';
-                    } else if ch == '0' {
-                        ch = ' ';
-                    }
                 }
 
                 let str: String;
@@ -216,7 +170,7 @@ impl<'a> Game<'a> {
 
             y += 1;
             x = 0;
-            for i in 0..self.board.size {
+            for _i in 0..self.board.size {
                 self.rustbox
                     .print(x, y, rustbox::RB_NORMAL, fg, bg, &boarder);
                 x += boarder.len();
@@ -229,13 +183,13 @@ impl<'a> Game<'a> {
         self.rustbox.present();
     }
 
-    fn run(&mut self) -> Status {
+    fn run(&mut self) {
         loop {
             self.draw();
 
             match self.rustbox.poll_event(false) {
                 Ok(rustbox::Event::KeyEvent(key)) => match key {
-                    RKey::Char('q') => return Status::QUIT,
+                    RKey::Char('q') => return,
                     RKey::Char('k') => {
                         if self.player_pos.0 == 0 {
                             self.player_pos.0 = self.board.size - 1;
@@ -264,71 +218,40 @@ impl<'a> Game<'a> {
                             self.player_pos.1 += 1;
                         }
                     }
-                    RKey::Char(' ') => {
-                        self.board.reveal_cell(self.player_pos.0, self.player_pos.1);
-                        if self.board.grid[self.player_pos.0][self.player_pos.1].is_bomb {
-                            return Status::LOST;
-                        }
-                    }
-                    RKey::Char('m') => {
-                        self.board
-                            .mark_cell_toggle(self.player_pos.0, self.player_pos.1);
+                    RKey::Char(' ') => self
+                        .board
+                        .alive_cell_toggle(self.player_pos.0, self.player_pos.1),
+                    RKey::Char('n') => {
+                        self.board.update();
                     }
                     _ => continue,
                 },
                 Err(e) => panic!("{}", e),
                 _ => panic!("Something happend"),
             };
-
-            if self.board.chech_win() {
-                return Status::WON;
-            }
         }
     }
 }
 
 fn main() {
-    let size = 20;
+    let size = 10;
     // let board = Board::new(size, 10);
     let rustbox = match RustBox::init(Default::default()) {
         Result::Ok(v) => v,
         Result::Err(e) => panic!("{}", e),
     };
 
-    let mut game = Game::new(size, 40, &rustbox);
-    match game.run() {
-        Status::WON => {
-            rustbox.print(
-                0,
-                0,
-                rustbox::RB_NORMAL,
-                Color::Green,
-                Color::Black,
-                "You Won!!!",
-            );
-        }
-
-        Status::LOST => {
-            rustbox.print(
-                0,
-                0,
-                rustbox::RB_NORMAL,
-                Color::Red,
-                Color::Black,
-                "You Lost!!!",
-            );
-        }
-
-        _ => return,
-    }
+    let mut game = Game::new(size, &rustbox);
+    game.run();
     rustbox.present();
-    loop {
-        match rustbox.poll_event(false) {
-            Ok(rustbox::Event::KeyEvent(key)) => match key {
-                _ => return,
-            },
-            Err(e) => panic!("{}", e),
-            _ => panic!("Something happend"),
-        }
-    }
+
+    // loop {
+    //     match rustbox.poll_event(false) {
+    //         Ok(rustbox::Event::KeyEvent(key)) => match key {
+    //             _ => return,
+    //         },
+    //         Err(e) => panic!("{}", e),
+    //         _ => panic!("Something happend"),
+    //     }
+    // }
 }
